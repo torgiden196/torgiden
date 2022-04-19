1e5c56569e8d395792f34e3859513ea0f4d6bb86
torgiden196 <102734405+torgiden196@users.noreply.github.com>
2022-04-19 12:45:15 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+(function(){
+  "use strict";
+  /* Start of your code */
+  function greetMe(yourName) {
+    alert('Hello ' + yourName);
+  }
+
+  greetMe('World');
+  /* End of your code */
+})();
